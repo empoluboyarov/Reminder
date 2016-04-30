dc9930921a8d5d4672b84c69c4f5ecd38fdbe8bf
empoluboyarov <poluboyarovem@gmail.com>
2016-04-30 14:34:59 +0300

    create Utils class for convert data/time from long to simple format

---
+++ app/src/main/java/com/empoluboyarov/reminder/Utils.java
@@ -0,0 +1,19 @@
+package com.empoluboyarov.reminder;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by Evgeniy on 30.04.2016.
+ */
+public class Utils {
+
+    public static String getData(long date){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yy");
+        return dateFormat.format(date);
+    }
+
+    public static String getTime(long time){
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH.mm");
+        return timeFormat.format(time);
+    }
+}
