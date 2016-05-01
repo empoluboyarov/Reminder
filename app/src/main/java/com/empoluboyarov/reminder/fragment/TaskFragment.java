c337ef14f47a16495092a688fe7bd839018d05e7
empoluboyarov <poluboyarovem@gmail.com>
2016-05-01 20:15:41 +0300

    create TaskAdapter.java

---
+++ app/src/main/java/com/empoluboyarov/reminder/fragment/TaskFragment.java
@@ -0,0 +1,9 @@
+package com.empoluboyarov.reminder.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Evgeniy on 01.05.2016.
+ */
+public abstract class TaskFragment extends Fragment{
+}
